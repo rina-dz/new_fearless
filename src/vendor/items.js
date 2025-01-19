52253cff4c19da8846f6837ed53b396ff99e4dab
rin-dz <dz.rina.na@gmail.com>
2025-01-19 21:55:16 +0300

    dev: создать переменные с карточками

---
+++ src/vendor/items.js
@@ -0,0 +1,51 @@
+// img={card} isPreorder={true} band='BTS/J-hope/Альбом' title='Альбом J-hope - Jack In The Box, HOPE Edition' price="2498₽"
+
+
+// {
+//     'img': '../../vendor/items/img-2.svg',
+//     'isPreorder': false,
+//     'band': '',
+//     'title': '',
+//     'price': ''
+// },
+
+
+let ItemsCards = [
+    {
+        'img': '../../vendor/items/img-1.svg',
+        'isPreorder': false,
+        'band': 'BTS/J-hope/Альбом',
+        'title': 'Альбом J-hope - Jack In The Box, HOPE Edition',
+        'price': '2498₽'
+    },
+    {
+        'img': '../../vendor/items/img-2.svg',
+        'isPreorder': false,
+        'band': 'StrayKids/Felix/Журнал',
+        'title': 'Журнал Harper’s BAZAAR 2024',
+        'price': '3590₽'
+    },
+    {
+        'img': '../../vendor/items/img-3.svg',
+        'isPreorder': false,
+        'band': 'aespa/Альбом',
+        'title': 'Альбом aespa - Girls',
+        'price': '2500₽'
+    },
+    {
+        'img': '../../vendor/items/img-4.svg',
+        'isPreorder': false,
+        'band': '(G)i-dle/YOQI/Альбом',
+        'title': 'Альбом YUQI ((G)I-DLE)1st Mini Album',
+        'price': '1980₽'
+    },
+    {
+        'img': '../../vendor/items/img-5.svg',
+        'isPreorder': false,
+        'band': 'Xdinary Heroes/Альбом',
+        'title': 'Альбом Xdinary Heroes Troubleshooting (PLATFORM ALBUM)',
+        'price': '2750₽'
+    },
+];
+
+export default ItemsCards;
---
+++ src/vendor/preorderItems.js
@@ -0,0 +1,47 @@
+// {
+//     'img': '../../vendor/items/img-2.svg',
+//     'isPreorder': false,
+//     'band': '',
+//     'title': '',
+//     'price': ''
+// },
+
+let PreorderItemsCards = [
+    {
+        'img': '../../vendor/items/img-6.svg',
+        'isPreorder': true,
+        'band': 'itzy/Другое',
+        'title': `Набор ITZY - 2025 Season's Greetings [Best Friends Forever]`,
+        'price': '3390₽'
+    },
+    {
+        'img': '../../vendor/items/img-7.svg',
+        'isPreorder': true,
+        'band': 'TWS/Альбом',
+        'title': 'Альбом TWS - 1st Single [Last Bell]',
+        'price': '2490₽'
+    },
+    {
+        'img': '../../vendor/items/img-8.svg',
+        'isPreorder': false,
+        'band': 'izna/Альбом',
+        'title': 'Альбом izna - 1st Mini Album [N/a]',
+        'price': '3170₽'
+    },
+    {
+        'img': '../../vendor/items/img-9.svg',
+        'isPreorder': true,
+        'band': 'StrayKids/Другое',
+        'title': `Набор Stray Kids - 2025 Season's Greeting [The Street Kids]`,
+        'price': '4120₽'
+    },
+    {
+        'img': '../../vendor/items/img-10.svg',
+        'isPreorder': true,
+        'band': 'TXT/Альбом',
+        'title': 'Альбом TOMORROW X TOGETHER - [THE STAR CHAPTER : SANCTUARY] ',
+        'price': '2999₽'
+    },
+]
+
+export default PreorderItemsCards;
